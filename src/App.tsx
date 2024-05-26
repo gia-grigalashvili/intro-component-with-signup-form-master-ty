bd6c3619eb54a134de3295d40991c30abeeb4ddd
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-26 19:34:05 +0400

    made maindiv in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import styled from "styled-components";
 import "./App.css";
-
+import Text from "./components/Text";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Maindiv>
+        <Text />
+      </Maindiv>
+    </>
+  );
 }
 
+const Maindiv = styled.div`
+  display: flex;
+`;
 export default App;
